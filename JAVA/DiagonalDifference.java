6a4d1ddcede622ba138fe1df15259512cbf627ed
Mario SAM <eu@mariosam.com.br>
2022-10-18 00:24:37 -0300

    Create DiagonalDifference.java

---
+++ JAVA/DiagonalDifference.java
@@ -0,0 +1,70 @@
+/**
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package JAVA;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import static java.util.stream.Collectors.toList;
+
+public class DiagonalDifference {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        int d1 = 0;
+        int d2 = 0;
+    
+        for ( int i = 0, j = arr.size()-1; i < arr.size(); i++, j-- ) {
+            //soma diagonal up-left to down-right
+            d1 += arr.get(i).get(i);
+    
+            //soma diagonal down-lef to up-right
+            d2 += arr.get(j).get(i);
+        }
+
+        return Math.abs(d1 - d2);
+    }
+}
