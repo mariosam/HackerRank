23c9fdbc4333d5bab782d31dabd284772413d37a
Mario SAM <eu@mariosam.com.br>
2022-10-27 02:31:26 -0300

    Create CoutingSort1.java

---
+++ JAVA/CoutingSort1.java
@@ -0,0 +1,65 @@
+/**
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package JAVA;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class CoutingSort1 {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        //int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = countingSort(arr);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+    /*
+     * Complete the 'countingSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+    public static List<Integer> countingSort(List<Integer> arr) {
+        //coloca os dados em ordem crescente
+        Collections.sort( arr );
+
+        //inicializa retorno com array zerado
+        int[] res = new int[100];
+
+        //percorre o array somando as quantidades
+        for ( int i = 0; i < arr.size(); i++ ) res[ arr.get(i) ]++;
+
+        return (ArrayList<Integer>) Arrays.stream(res).boxed().collect(Collectors.toList());
+    }
+}
